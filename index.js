cf128850cef7a1decbb7924079c7701db7def3fe
Geniye1 <jstrayer57@gmail.com>
2023-09-12 14:47:26 -0500

    Added more variability to the response messages

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+// Listen, I know I'm using .innerHTML. I fucking get it. It's inefficient. It's a security risk. 
+// I fully understand why you shouldn't use it. But this is a rock, paper, scissors game. 
+// It literally does not matter. Go to hell.
+
 'use strict'
 
 function getComputerChoice() {
-    let choices = ["Rock", "Paper", "Scissors"];
-    let randomChoice = Math.floor(Math.random() * choices.length);
-    console.log(choices[randomChoice]);
-    return choices[randomChoice];
+    let randomChoice = Math.floor(Math.random() * computerChoices.length);
+    console.log(computerChoices[randomChoice]);
+    return computerChoices[randomChoice];
 }
 
 function playRound(userChoiceIn) {
@@ -12,32 +15,71 @@ function playRound(userChoiceIn) {
     // Ensure the user isn't pulling a fast one on me
     if (isNaN(userChoiceIn)) {
         if (userChoiceIn == computerChoice) {
-            resultText.textContent = "We seem to have chosen the same thing. So, how long have you be a follower and not a leader?";
+            resultText.innerHTML = "We seem to have chosen the same thing. So, how long have you be a follower and not a leader?";
             return tieReturn;
         }
         else {
             if (userChoiceIn == "paper" && computerChoice == "rock") {
-                resultText.textContent = winMessage;
+                if (previousLossMessageIndex == 5) previousLossMessageIndex = 0;
+
+                let winMessageIndex = Math.floor(Math.random() * winMessages.length);
+                resultText.innerHTML = winMessages[winMessageIndex];
                 return playerWinsReturn;
             }
             else if (userChoiceIn == "rock" && computerChoice == "scissors") {
-                resultText.textContent = winMessage;
+                if (previousLossMessageIndex == 5) previousLossMessageIndex = 0;
+
+                let winMessageIndex = Math.floor(Math.random() * winMessages.length);
+                resultText.innerHTML = winMessages[winMessageIndex];
                 return playerWinsReturn;
             }
             else if (userChoiceIn == "scissors" && computerChoice == "paper") {
-                resultText.textContent = winMessage;
+                if (previousLossMessageIndex == 5) previousLossMessageIndex = 0;
+
+                let winMessageIndex = Math.floor(Math.random() * winMessages.length);
+                resultText.innerHTML = winMessages[winMessageIndex];
                 return playerWinsReturn;
             }
             else if (userChoiceIn == "scissors" && computerChoice == "rock") {
-                resultText.textContent = loseMessage;
+                if (previousLossMessageIndex == 5) {
+                    resultText.innerHTML = bluffLossResponse;
+                    previousLossMessageIndex = 0;
+                }
+                else {
+                    let lossMessageIndex = Math.floor(Math.random() * loseMessages.length);
+                    resultText.innerHTML = loseMessages[lossMessageIndex];
+                    previousLossMessageIndex = lossMessageIndex;
+                    console.log(previousLossMessageIndex);
+                }
+                
                 return computerWinsReturn;
             }
             else if (userChoiceIn == "paper" && computerChoice == "scissors") {
-                resultText.textContent = loseMessage;
+                if (previousLossMessageIndex == 5) {
+                    resultText.innerHTML = bluffLossResponse;
+                    previousLossMessageIndex = 0;
+                }
+                else {
+                    let lossMessageIndex = Math.floor(Math.random() * loseMessages.length);
+                    resultText.innerHTML = loseMessages[lossMessageIndex];
+                    previousLossMessageIndex = lossMessageIndex;
+                    console.log(previousLossMessageIndex);
+                }
+
                 return computerWinsReturn;
             }
             else if (userChoiceIn == "rock" && computerChoice == "paper") {
-                resultText.textContent = loseMessage;
+                if (previousLossMessageIndex == 5) {
+                    resultText.innerHTML = bluffLossResponse;
+                    previousLossMessageIndex = 0;
+                }
+                else {
+                    let lossMessageIndex = Math.floor(Math.random() * loseMessages.length);
+                    resultText.innerHTML = loseMessages[lossMessageIndex];
+                    previousLossMessageIndex = lossMessageIndex;
+                    console.log(previousLossMessageIndex);
+                }
+
                 return computerWinsReturn;
             }
             else {
@@ -80,11 +122,11 @@ function endGame() {
         gameEndHeader.textContent = "WINNER!";
         gameEndMessage.textContent = "FUCK YOU!!!!! I HATE LOSING!!!! AND I HATE YOU!!!! BURN IN HELL!!!!!!!;"
     }
-    else if (roundsWin < roundsLost) {
+    else if (roundsWon < roundsLost) {
         gameEndHeader.textContent = "LOSER!";
         gameEndMessage.textContent = "I knew I would win. You are so predictable!";
     }
-    else if (roundsWin == roundsLost) {
+    else if (roundsWon == roundsLost) {
         gameEndHeader.textContent = "TIE!";
         gameEndMessage.textContent = "It would seem we are evenly matched. However, I did that on purpose. I'm just too good to be beaten";
     }
@@ -98,16 +140,30 @@ function resetGame() {
     playerScore.textContent = "0";
     geribaldiScore.textContent = "0";
     gameEndContainer.style.visibility = "hidden";
-
-    // I know I shouldn't use this, but its run ONCE so its not that innefficient and the 
-    // user can't submit the text here so FUCK YOU IM USING IT
     resultText.innerHTML = initialMessage;
 }
 
+let computerChoices = ["Rock", "Paper", "Scissors"];
+
 const initialMessage = "Good evening fellow citizen. It would seem that you have challenged me to a game of rock, paper, scissors. " + 
     "Whether it is friendly or not will be decided by the outcome of the game.<br>I do <span id='not-span'>not</span> like losing."
-const winMessage = "You seem to have bested me, it won't happen again though. \nChoose again, NOW!!!";
-const loseMessage = "You have failed, I clearly am the superior being. Choose again, not that it matters.";
+const winMessage = "You seem to have bested me, it won't happen again though. <br>Choose again, NOW!!!";
+const loseMessage = "You have failed, I clearly am the superior being. <br>Choose again, not that it matters.";
+
+const winMessages = ["You seem to have bested me, it won't happen again though. <br>Choose again, NOW!!!",
+                     "Did you hack this website? Can you see what I will choose? Surely you must be, there's no other explanation. <br>Choose again, cheater.",
+                     "Do you brag to people that you're good at this game? I bet you do. <br>Choose again before I lose my shit.",
+                     "You better thank whatever God you pray to for the CORS policy, otherwise I'd fill your entire hard drive with millions of images of wombats until your computer blue screens. <br> Choose again.",
+                     "Oh wow, you beat a computer at a game where you have a 50% chance of winning and a 33% of tying. Wow, so impressive! <br> Choose again you pompous FUCK.",
+                     "I have nothing witty to say. Simply go fuck yourself. <br> Choose again."];
+
+const loseMessages = ["You have failed, I clearly am the superior being. <br>Choose again, not that it matters.",
+                      "Wow... 200,000 years of evolution and you lost a game of rock, paper, scissors to essentially a rock. Now THAT'S funny. <br> Choose again.",
+                      "You humans spend roughly 1/3 of your lives sleeping, and yet you chose to spend your precious leftover 2/3's on losing to a computer. HAHAHAH!!!!! <br> Choose again, fool.",
+                      "You do know the goal is to beat me right? I just wanted to make sure you knew that. <br> Choose again, <em>correctly</em> this time.",
+                      "Wow, you really suck at this. Maybe go outside and take a second to reconsider your strategy moving forward. <br> Are you done? Choose again.",
+                      "Here's what I'm going to do, since you clearly are not good at this I'll tell you right now: I'm choosing " + 
+                        computerChoices[Math.floor(Math.random() * computerChoices.length)] + " next time. Am I lying? <br>Call my bluff you meatsack."];
 
 // Return strings for determining the outcome of the round
 const playerWinsReturn = "PLAYER_WINS";
@@ -127,7 +183,12 @@ const resetButton = document.querySelector("#reset-button");
 const gameEndHeader = document.querySelector("#game-result-header");
 const gameEndMessage = document.querySelector("#game-result-message");
 
-const numberOfRounds = 4;
+// This is purely because loseMessages[5] kind of implies context to the next choice, which if it is wrong then 
+// it needs to properly respond and not repeat it
+let previousLossMessageIndex = 0;
+let bluffLossResponse = "HAHAHA!!! OF COURSE I WAS LYING!!!! FOOLISH HUMAN!!! HAHAHAHHA ok, ok... I'm ok. That was really funny. <br>Alright, choose again.";
+
+const numberOfRounds = 20;
 let totalRounds = 1;
 let roundsWon = 0;
 let roundsLost = 0;
